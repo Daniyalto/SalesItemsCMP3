6fb9d6c6b9c66965479172fe0b9ed2116dd959d1
Daniyalto <160604470+Daniyalto@users.noreply.github.com>
2025-12-19 08:30:05 +0100

    Update KoinModel.kt

--- composeApp/src/commonMain/kotlin/org/example/project/Model/KoinModel.kt
+++ composeApp/src/commonMain/kotlin/org/example/project/Model/KoinModel.kt
@@ -8,16 +8,16 @@ import org.koin.dsl.module
 import org.koin.core.context.startKoin
 
 val appModule = module {
-    // Singletons
+    
     single { ItemService() }
     single<ItemsRepository> { ItemsRepositoryImpl(itemService = get()) }
 
-    // Factories (View Models)
+    
     factory { SaleItemViewModel(repository = get()) }
     factory { AuthViewModel() }
 }
 
-// Global funktion til at starte Koin i native code (kaldes fra MainActivity.kt / iOS Swift)
+
 fun initKoin() {
     startKoin {
         modules(appModule)
